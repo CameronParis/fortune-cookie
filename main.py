12614efbda64d93518a39251399d45d93ca4c167
Cameron Paris <Cameron.Paris@gmail.com>
2017-03-23 20:00:45 -0500

    Created Fortune and Lucky Number Functions

--- main.py
+++ main.py
@@ -15,10 +15,37 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+    fortunes = [
+        "Always take the path less traveled; there's less traffic.",
+        "Early to bed, early to rise, makes a man an anomaly these days.",
+        "Two wrongs don't make a right, but two Wrights make a plane.",
+        "Fortunes are earned, not given; however, they can be taken."
+    ]
+
+    return random.choice(fortunes)
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        header = "<h1>Fortune Cookie</h1>"
+
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentence = 'Your fortune: ' + fortune
+        fortune_paragraph = '<p>' + fortune_sentence + '</p>'
+
+        lucky_number = "<strong>" + str(random.randint(1,100)) + "</strong>"
+        number_sentence = 'Your lucky number: ' + lucky_number
+        number_paragraph = '<p>' + number_sentence + '</p>'
+
+        new_cookie_button = '<a href="."><button>Another cookie, please!</button></a>'
+
+        content = header + fortune_paragraph + number_paragraph + new_cookie_button
+
+        self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
